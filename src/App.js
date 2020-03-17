4e41cd001d9c5b2c7e32111fa196b0e83d1d20ea
LuciaGutierrez <lucia.gutierrez@soprasteria.com>
2020-03-17 14:18:01 +0100

    Conexión formulario y API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css'
+import { Title } from './components/Title'
+import { SearchForm } from './components/SearchForm'
 
+class App extends Component {
+  state = { results: [] }
 
-function App() {
-  return (
-    <div className="App">
-      Search Movies
-    </div>
-  );
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
+  _renderResults () {
+    const {results} = this.state
+    return results.map(movie => {
+    return <p key={movie.imdbID}>{movie.Title}</p>
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title>Search Movies</Title> {/*Pasamos Search Movies como children*/}
+        <div className='SearchForm-wrapper'>
+          <SearchForm onResults={this._handleResults} />
+        </div>
+        {this.state.results.length === 0
+          ? <p>Sin resultados</p>
+          : this._renderResults()}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+const API_KEY = '6fb7f564'
 export class SearchForm extends Component {
 
     state = {
@@ -12,20 +12,29 @@ export class SearchForm extends Component {
 
     _handleSubmit = (e) => {
         e.preventDefault()
-        alert(this.state.inputMovie)
+        const {inputMovie} = this.state
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`) //s hace una busqueda y devuelve un listado con todos los que coincidan con el nombre
+        .then(res => res.json()) //devuelve una promesa
+        .then(results => {
+            //console.log(results)
+            const {Search, totalResults} = results
+            console.log({Search, totalResults})
+            this.props.onResults(Search)
+        })
     }
+    
     render() {
         return (
             <form onSubmit={this._handleSubmit}>
-                <div class="field has-addons">
-                    <div class="control">
+                <div className="field has-addons">
+                    <div className="control">
                         <input
                             onChange={this._handleChange}
-                            class="input"
+                            className="input"
                             type="text"
                             placeholder="Movie to search..." />
                     </div>
-                    <div class="control">
+                    <div className="control">
                         <button className="button is-info">
                             Search
                         </button>
