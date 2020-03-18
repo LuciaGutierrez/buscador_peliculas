b4b396d9deb2538b0fa0bd6aa9720ab6bec811f1
LuciaGutierrez <lucia.gutierrez@soprasteria.com>
2020-03-18 10:12:24 +0100

    Añadiendo página Home

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css'
-import { Title } from './components/Title'
-import { SearchForm } from './components/SearchForm'
-import { Movie } from './components/Movie'
-import { MoviesList } from './components/MoviesList';
 import {Detail} from './pages/Detail'
+import {Home} from './pages/Home'
+
 
 class App extends Component {
-  state = { usedSearch: false,results: [] }
+  
+  render() {
+    const url = new URL(document.location)
+    const Page = url.searchParams.has('id')
+    ? <Detail id = {url.searchParams.get('id')}/>
+    : <Home />
 
+    return (
+      <div className="App">
+        {Page}
+      </div>
+    );
+  }
+}
+
+
+/*
   _handleResults = (results) => {
     this.setState({ results, usedSearch:true })
   }
-  /*
+  
     _renderResults () {
       const {results} = this.state
       return results.map(movie => {
@@ -26,32 +39,12 @@ class App extends Component {
       )
       })
     }
-  */
+  
   _renderResults() {
     return this.state.results.length === 0
       ? <p>Sorry! Results not found</p>
       : <MoviesList movies={this.state.results} />
     }
-  
-  render() {
-    const url = new URL(document.location)
-    const hasId = url.searchParams.has('id')
-    if (hasId){
-      return <Detail id = {url.searchParams.get('id')}/>
-    }
-    return (
-      <div className="App">
-        <Title>Search Movies</Title> {/*Pasamos Search Movies como children*/}
-        <div className='SearchForm-wrapper'>
-          <SearchForm onResults={this._handleResults} />
-        </div>
-        {this.state.usedSearch
-          ? this._renderResults()
-          : <small>Use the form to search a movie</small>}
-
-      </div>
-    );
-  }
-}
+  */
 
 export default App;
